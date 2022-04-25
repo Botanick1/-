98ebb9e9fb249e8d67d7e45eeb5b96f354ec3a02
Botanick1 <97731093+Botanick1@users.noreply.github.com>
2022-04-25 17:41:01 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Калькулятор_Матриц1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(103, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сложение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(446, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 63);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Вычитание";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(103, 230);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(198, 63);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Определитель";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(446, 230);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(198, 63);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Ранг";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(446, 344);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(198, 63);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Обратная матрица";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(103, 115);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(198, 63);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Умножение ";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(103, 344);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(198, 63);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Транспонирование";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(446, 115);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(198, 63);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Умножение на число";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.Crimson;
+            this.Name = "Form1";
+            this.Text = "Калькулятор Матриц";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Form2.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Калькулятор_Матриц1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text.Length > 1) //устанавливаю размер матрицы не больше 9
+            {
+                MessageBox.Show("Слишком большое количество столбцов!");
+                textBox1.Clear();
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 48 || e.KeyChar >= 54) && number != 8) // цифры и удаление
+            {
+                e.Handled = true;
+            }
+           
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 48 || e.KeyChar >= 54) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int row = Convert.ToInt32(textBox2.Text); //считываю колво строк
+            int column = Convert.ToInt32(textBox1.Text); // считываю колво столбцов
+            double[,] matrix = new double[row, column]; //создаю матрицу
+            panel1.Controls.Clear(); // очистка предыдущей матрицы
+            panel2.Controls.Clear();
+            double[] MatrixA = new double[row * column];
+            double[] MatrixB = new double[row * column];
+            for ( int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                { 
+         
+                    var textBox3 = new TextBox();
+                    double a = Convert.ToDouble(textBox3.Text);
+                    MatrixA[i] = a;
+                    textBox3.Location = new Point(i * 30, j * 30);  // матрица A в текстбоксах
+                    textBox3.Size = new Size(26, 26);
+                    panel1.Controls.Add(textBox3);
+                    var textBox4 = new TextBox();
+                    double b = Convert.ToDouble(textBox4.Text);
+                    MatrixB[i] = b;
+                    textBox4.Location = new Point(i * 30, j * 30);  // матрица B в текстбоксах
+                    textBox4.Size = new Size(26, 26);
+                    panel2.Controls.Add(textBox4);
+                    Label label = new Label();
+                    label.Location = new Point(i * 30, j * 30);
+                    label.Size = new Size(26, 26);
+                    panel3.Controls.Add(label);
+                    
+
+
+                }
+            }
+
+           
+            
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length > 1)
+            {
+                MessageBox.Show("Слишком большое количество строк!"); 
+                textBox2.Clear();
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+    }
+}
